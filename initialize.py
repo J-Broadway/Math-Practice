e7a4c43b898961f77f7eb9da2048db06a967e299
J-Broadway <jeremiah.broadway@hotmail.com>
2019-07-14 22:52:49 -0500

    Edited initialize.py

--- initialize.py
+++ initialize.py
@@ -2,10 +2,9 @@ import pickle
 from os import path
 
 
-# Check for ranges.pickle If not available create it
+# Check for ranges.pickle. If not available create it.
 def main():
-    ranges = {'a': [[1, 100], [1, 100]], 's': [[1, 100], [1, 100]], 'm': [[1, 10], [1, 10]], 'd': [[1, 100], [1, 100]]}
-
+    ranges = {'a': [[1, 100], [1, 100]], 's': [[1, 100], [1, 100]], 'm': [[1, 12], [1, 12]], 'd': [[1, 12], [1, 12]]}
     if path.exists("ranges.pickle") == True:
         pass
     else:
